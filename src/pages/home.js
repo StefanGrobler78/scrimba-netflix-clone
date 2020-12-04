c60dc0367d972a53d589a0c796f9f3075d685204
Stefan Grobler <stefangrobler78@gmail.com>
2020-12-04 12:41:50 +0200

    Jumbotron Created

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { JumbotronContainer } from "../containers/JumbotronContainer"
+
+export default function Home() {
+    return(
+        <JumbotronContainer>
+
+        </JumbotronContainer>
+    )
+}
